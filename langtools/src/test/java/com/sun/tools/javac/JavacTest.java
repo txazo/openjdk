a226aa06bbb70d126d474603f63ea4ce65f34040
txazo <784990655@qq.com>
2020-05-10 19:12:14 +0800

    update

---
+++ langtools/src/test/java/com/sun/tools/javac/JavacTest.java
@@ -0,0 +1,9 @@
+package com.sun.tools.javac;
+
+public class JavacTest {
+
+    public static void main(String[] args) throws Exception {
+        Main.main(new String[]{"/Users/txazo/TxazoProject/java/src/main/java/org/txazo/jdk8/lambda/LamdbaGenarateTest.java"});
+    }
+
+}
